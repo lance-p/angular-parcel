2938299eabffabc3c5377808a682869bce112df6
Jack DeMeyers <jackson.demeyers@gmail.com>
2017-12-10 14:14:48 -0500

    renamed and reorganized Angular application

--- src/ClientApp/main.ts
+++ src/ClientApp/main.ts
@@ -2,6 +2,6 @@ import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import { platformBrowserDynamic } 
                      from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/components/app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
