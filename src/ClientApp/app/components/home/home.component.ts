2ec4e3075a968a39bdfc3e6f6ad176196a2c1b23
Jack DeMeyers <jackson.demeyers@gmail.com>
2017-12-10 18:16:07 -0500

    remove tsc tasks, as parcel handles this.

--- src/ClientApp/app/components/home/home.component.ts
+++ src/ClientApp/app/components/home/home.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: '<div class="jumbotron"><div class="container text-center"><h1 class="display-3">angular-parcel</h1><p class="lead">Boilerplate Angular application bundled with Parcel.</p></div></div>'
+  template: '<div class="jumbotron"><div class="container text-center"><h1 class="display-3">{{name}}</h1><p class="lead">Boilerplate Angular application bundled with Parcel.</p></div></div>'
 })
 
 export class HomeComponent {
-    name = "Jackson";
+    name = "angular-parcel";
 }
