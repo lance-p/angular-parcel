d4545820be85193d8c736376f6d7e29c40a89fa5
Jack DeMeyers <jackson.demeyers@gmail.com>
2017-12-10 15:46:42 -0500

    es6

--- src/ClientApp/app/components/app/app.component.ts
+++ src/ClientApp/app/components/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  template: '<h1>Hello, {{name}}</h1>'
+  template: '<h1>Hello, {{name}}<h1/>'
 })
 
 export class AppComponent {
