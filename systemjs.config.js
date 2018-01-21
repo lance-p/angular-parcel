1ae95a2b30aa83c05655a01a11e98796bc7091a2
Jack DeMeyers <jackson.demeyers@gmail.com>
2018-01-21 16:00:27 -0500

    update systemjs.config

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,12 +5,15 @@ System.config({
     map: {
         app: 
         'dist/',
-        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
         '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
         '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/compiler-cli': 'npm:@angular/compiler-cli/bundles/compiler-cli.umd.js',
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
         '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
         '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
         '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
         'core-js': 'npm:core-js',
         'zone.js': 'npm:zone.js',
